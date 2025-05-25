78665346a17d8079667f70ac1f8349691c791fcc
Minsup Lee <m1nsuppp@gmail.com>
2025-05-25 23:14:40 +0900

    feat: transformation

--- playgrounds/r3f/src/App.tsx
+++ playgrounds/r3f/src/App.tsx
@@ -1,45 +1,48 @@
 import { useState, useRef, type JSX } from "react";
 import "./App.css";
-import { Canvas, useFrame, type ThreeElements } from "@react-three/fiber";
-import type { Mesh } from "three";
+import { Canvas } from "@react-three/fiber";
+import { Mesh, Vector3 } from "three";
+import { OrbitControls } from "@react-three/drei";
 
-function Box(props: ThreeElements["mesh"]): JSX.Element {
+function Box2() {
   const meshRef = useRef<Mesh>(null!);
   const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  useFrame((_, delta) => (meshRef.current.rotation.x += delta));
+  const [position, setPosition] = useState<Vector3>(new Vector3(0, 0, 0));
 
   return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "#2f74c0"} />
-    </mesh>
+    <>
+      <directionalLight position={[1, 1, 1]} />
+      <axesHelper scale={10} />
+      <OrbitControls />
+      <mesh
+        ref={meshRef}
+        position={position}
+        onClick={() => {
+          setPosition(new Vector3(position.x + 1, position.y, position.z));
+        }}
+        onPointerOver={() => {
+          setHover(true);
+          setPosition(new Vector3(1, 1, 1));
+        }}
+        onPointerOut={() => {
+          setHover(false);
+          setPosition(new Vector3(0, 0, 0));
+        }}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color={hovered ? "orange" : "hotpink"} />
+      </mesh>
+    </>
   );
 }
 
 function App(): JSX.Element {
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas>
+        <Box2 />
+      </Canvas>
+    </div>
   );
 }
 
