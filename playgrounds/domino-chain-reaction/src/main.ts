ae4cb7327c8d48232c2dbe0e14a965ab7d8f3468
Minsup Lee <m1nsuppp@gmail.com>
2025-06-02 00:10:47 +0900

    feat: geometries

--- playgrounds/domino-chain-reaction/src/main.ts
+++ playgrounds/domino-chain-reaction/src/main.ts
@@ -12,17 +12,46 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(-3, 0, 0);
+scene.add(box);
 
-camera.position.z = 5;
+const planeGeometry = new THREE.PlaneGeometry(0.5, 0.5);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.set(3, 0, 0);
+scene.add(plane);
+
+const sphereGeometry = new THREE.SphereGeometry();
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(0, 0, 0);
+scene.add(sphere);
+
+const cylinderGeometry = new THREE.CylinderGeometry();
+const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+cylinder.position.set(0, 0, 0);
+scene.add(cylinder);
+
+camera.position.set(0, 0, 5);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
+
+  plane.rotation.x += 0.01;
+  plane.rotation.y += 0.01;
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+
+  cylinder.rotation.x += 0.01;
+  cylinder.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
 
